d34b98ebfac5bbf854fa48648b40af1489970095
Paulo Ricardo <paulo_tolkien@outlook.com>
2023-03-06 21:50:16 -0300

    Configurando endpoints iniciais

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,61 +1,85 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DevFreela.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/projects")]
-    public class ProjectsController : ControllerBase
-    {
-        // api/projects?query=net core
-        [HttpGet]
-        public IActionResult Get(string query)
-        {
-            // Necessary to implement the query search flow.
+	public class ProjectsController : ControllerBase
+	{
+		private OpeningTimeOption _options;
+		public ProjectsController(IOptions<OpeningTimeOption> options)
+		{
+			_options = options.Value;
+		}
+		
+		// api/projects?query=net core
+		[HttpGet]
+		public IActionResult Get(string query)
+		{
+			// Necessary to implement the query search flow.
 
-            return Ok();
-        }
+			return Ok();
+		}
 
-        // api/projects/1
-        [HttpGet("{id}")]
-        public IActionResult GetById(int id)
-        {
-            // Need to implement the search flow by id
+		// api/projects/1
+		[HttpGet("{id}")]
+		public IActionResult GetById(int id)
+		{
+			// Need to implement the search flow by id
 
-            return Ok();
-        }
+			return Ok();
+		}
 
-        [HttpPost]
-        public IActionResult Post([FromBody] CreateProjectModel createProject)
-        {
-            if(createProject.Title.Length > 50) return BadRequest();
+		[HttpPost]
+		public IActionResult Post([FromBody] CreateProjectModel createProject)
+		{
+			if (createProject.Title.Length > 50) return BadRequest();
 
-            // Necessary to implement the flow of creating a projectModel.
+			// Necessary to implement the flow of creating a projectModel.
 
-            return CreatedAtAction(nameof(GetById), new {id = createProject.Id}, createProject);
-        }
+			return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+		}
 
-        // api/projects/2
-        [HttpPut("{id}")]
-        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
-        {
-            if(updateProject.Description.Length > 200) return BadRequest();
-            
-            // Need to implement the update flow.
+		// api/projects/2
+		[HttpPut("{id}")]
+		public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+		{
+			if (updateProject.Description.Length > 200) return BadRequest();
 
-            return NoContent();
-        }
+			// Need to implement the update flow.
 
-        // api/projects/3
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            // necessary to implement the flow to inactivate the projectModel
+			return NoContent();
+		}
 
-            return NoContent();
-        }
-    }
+		// api/projects/3
+		[HttpDelete("{id}")]
+		public IActionResult Delete(int id)
+		{
+			// necessary to implement the flow to inactivate the projectModel
+
+			return NoContent();
+		}
+
+		// api/projects/1/comments
+		[HttpPost("{id}/comments")]
+		public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComment)
+		{
+			return NoContent();
+		}
+
+		// api/projects/1/start
+		[HttpPut("{id}/start")]
+		public IActionResult Start(int id)
+		{
+			return NoContent();
+		}
+
+		// api/projects/1/finish
+		[HttpPut("{id}/finish")]
+		public IActionResult Finish(int id)
+		{
+			return NoContent();
+		}
+	}
 }
--- DevFreela.API/Startup.cs
+++ DevFreela.API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DevFreela.API.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -14,46 +15,47 @@ using Microsoft.OpenApi.Models;
 
 namespace DevFreela.API
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.Configure<OpeningTimeOption>(Configuration.GetSection("OpeningTime"));
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
-            });
-        }
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "DevFreela.API", Version = "v1" });
+			});
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DevFreela.API v1"));
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
